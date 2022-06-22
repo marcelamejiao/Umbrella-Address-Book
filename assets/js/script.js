40f78d07a1258b8f521c252984bb804a6d4778ea
Alex <alexander.h.perry@gmail.com>
2022-06-22 22:06:39 +1000

    Adjustments to function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,34 @@
 
 
 
-// Weather API function
-// lat/long from google maps api
-// To do:
-// Icons to match weather type and temperature
-// Umbrella for rain
-// shirt for sunny and warm
-// jumper/hoody for cold
-//
 
-var testObject; // Just for testing in console - data from the weather api fetch, can work with the object in console easier.
+
+
+
+// ##################################
+// checkWeather
+// Args: Latitude, Longitude
+// Using Weather API from: https://www.weatherapi.com/
+// JSON response with current weather data
+//  data.current.temp_c = dec - temperature in celcius
+//  data.current.condition.text = string - current weather condition
+//  data.current.is_day = bool - 1=day, 0=night
 // TODO:
-// conditionals for Icons - path is almost the same as in the object under data.current.condition - can get the icon details from this. 
-// Conditionals for additional requirements (ie. umbrella, jumper etc...)
+// Add icons to function - ./assets/weather/64x42/...
+//  --conditional checks for day/night and condition...
+// Add conditionals for Weather for Umbrella, jacket, etc.
+// ##################################
+var weatherTestObj; // FOR CONSOLE TESTING ONLY
 function checkWeather(lat, long) {
-  
     var apiUrl = "https://api.weatherapi.com/v1/current.json?key=6ae7c76b7b7d498db7a75709222206&q=" + lat + ", " + long + "&aqi=no";
-
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
-                // console.log(response);
                 response.json().then(function (data) {
                     var weatherTemp = data.current.temp_c; // Current Temperature in celcius. 
                     var weatherCondition = data.current.condition.text; // Weather condition, ie 'Clear'
                     var daynight
-                    testObject = data; /// <-------  Just for testing in console. Remove before release 
+                    weatherTestObj = data; /// <-------  Just for testing in console. Remove before release 
                     console.log("Current Temperature: ", weatherTemp);
                     console.log("Current Condition:", weatherCondition);
                     document.getElementById("weather-temp").textContent = weatherTemp + "°C";
@@ -44,5 +46,5 @@ function checkWeather(lat, long) {
             console.log('Unable to connect to WeatherAPI'); /// Remove alert before release
         });
 }
-
+/// Calling Function (replace the actual latitude and longitude with a variable from Google maps API)
 checkWeather(-33.8481188, 150.9262877);
