3aff1e44694f8aaf8cab37064487907cbcb7eac4
dansmith09 <dansmith_09@hotmail.com>
2022-06-23 20:16:44 +1000

    Render Map from address funcationality complete ready to merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,33 @@
-// Dan's Code :)
-
-
 // Rendering a map from Google Maps API
 
 // Defining my API Key
 var apiKey = "AIzaSyDSVjMQM3Hgp3upVIWiHSW1CTTP-VFT85A";
 
-// Converting address to Lat and Long values
-function getCoordinates(address){
+// Converts address to Lat and Long values and renders Map
+function addressToMap(address){
     fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + '&key=' + apiKey)
       .then(response => response.json())
       .then(data => {
-          console.log(data);
-        const lat = data.results[0].geometry.location.lat;
-        const lng = data.results[0].geometry.location.lng;
-        console.log({lat, lng})
-        initMap(lat,lng);
+        // Gets the address longitude and latitude coordinates
+        var lat = data.results[0].geometry.location.lat;
+        var lng = data.results[0].geometry.location.lng;
+        // Calls the render map function based on the coordinates of the given address
+        renderMap(lat,lng);
       })
 }
 
-
 // Initialize and add the map
-function initMap(lat, lng) {
-    // The location of user data
+function renderMap(lat, lng) {
+    // The location of user contact in longitude and latitude
     const contactLocation = { lat: lat, lng: lng };
-    // The map, centered at Uluru
+    // Creates a map based on the lng and lat coordinates above
     const map = new google.maps.Map(document.getElementById("location-map"), {
-      zoom: 4,
-      center: contactLocation,
+        // zooms into users locatioin
+        zoom: 12,
+        // centers location on map
+        center: contactLocation,
     });
-    // The marker, positioned at Uluru
+    // Creates a marker and puts it on the user contact's location on the map rendered above
     const marker = new google.maps.Marker({
       position: contactLocation,
       map: map,
