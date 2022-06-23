a4eef7ad30eb76aa876cab9e3c55653c25fd123a
dansmith09 <dansmith_09@hotmail.com>
2022-06-23 19:56:11 +1000

    Now converts Address to Lat and Lng and renders map based on coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,38 @@
 // Dan's Code :)
 
+
+// Rendering a map from Google Maps API
+
+// Defining my API Key
 var apiKey = "AIzaSyDSVjMQM3Hgp3upVIWiHSW1CTTP-VFT85A";
 
+// Converting address to Lat and Long values
+function getCoordinates(address){
+    fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + '&key=' + apiKey)
+      .then(response => response.json())
+      .then(data => {
+          console.log(data);
+        const lat = data.results[0].geometry.location.lat;
+        const lng = data.results[0].geometry.location.lng;
+        console.log({lat, lng})
+        initMap(lat,lng);
+      })
+}
+
+
 // Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
+function initMap(lat, lng) {
+    // The location of user data
+    const contactLocation = { lat: lat, lng: lng };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("location-map"), {
       zoom: 4,
-      center: uluru,
+      center: contactLocation,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: contactLocation,
       map: map,
     });
-  }
+}
   
-  window.initMap = initMap;
