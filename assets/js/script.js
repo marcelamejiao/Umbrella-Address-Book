86df7241956894451ef78341368d88190fc87ba2
Alex <alexander.h.perry@gmail.com>
2022-06-28 18:51:40 +1000

    Weather function buillt, sets the html of the div with id=weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,27 +13,37 @@
 //  --conditional checks for day/night and condition...
 // Add conditionals for Weather for Umbrella, jacket, etc.
 // ##################################
-
 function checkWeather(lat, long) {
     var apiUrl = "https://api.weatherapi.com/v1/current.json?key=6ae7c76b7b7d498db7a75709222206&q=" + lat + ", " + long + "&aqi=no";
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
-                response.json().then(function (data) {                     
+                response.json().then(function (data) {
                     var weather = {
                         temp: data.current.temp_c,
                         condition: data.current.condition.text,
                         icon: data.current.condition.icon,
                         rain: data.current.condition.text.includes("rain")
                     };
-                    return (weather);
+                    // Create the html string based off temperature and condition - temperature-condition image - temperature image - umbrella image
+                    var weatherString = weather.temp + '°C' + '<img src=http:' + weather.icon + '></img>';
+                    if (weather.temp <= 16) {
+                        weatherString += '<img src=./assets/images/snowflake.png>';
+                    } else if (weather.temp > 16 && weather.temp < 24) {
+                        weatherString += '<img src=./assets/images/jacket.png>';
+                    } else if (weather.temp >= 24 && weather.temp <= 32) {
+                        weatherString += '<img src=./assets/images/shirt.png>';
+                    } else if (weather.temp > 32) {
+                        weatherString += '<img src=./assets/images/fire.png>';
+                    };
+                    //Check for rain
+                    if (weather.rain) {
+                        weatherString += '<img src=./assets/images/umbrella.png>';
+                    };
+                    // Set the div element id: weather with the html built with weatherString
+                    document.getElementById("weather").innerHTML = weatherString;
                 });
-            } else {
-                console.log('Error: ' + response.statusText); //Remove alerts before release
             }
-        })
-        .catch(function (error) {
-            console.log('Unable to connect to WeatherAPI'); /// Remove alert before release
         });
 };
 
@@ -41,31 +51,31 @@ var state = {
     contacts: []
 };
 
-function init () {
+function init() {
     // Load the state object from local storage
     loadState();
     //Show the conctact list
     renderContactList();
 
-    $("#add-button").on("click",newContact);
-    $("#save-button").on("click",saveContact);
+    $("#add-button").on("click", newContact);
+    $("#save-button").on("click", saveContact);
 
 }
 
 function renderContactList() {
     var contactList = $("#contact-list ul");
 
-    for (var i= 0; i<state.contacts.length; i++){
+    for (var i = 0; i < state.contacts.length; i++) {
         var contact = state.contacts[i];
-       
-        var listItem = $("<li>"+contact.firstName+" "+contact.lastName+"</li>");
+
+        var listItem = $("<li>" + contact.firstName + " " + contact.lastName + "</li>");
         contactList.append(listItem);
 
     }
 }
 
 function newContact() {
-    
+
     $("#contact-information").removeClass("d-none");
 }
 
@@ -81,10 +91,10 @@ function saveContact(event) {
 
     var contact = {
         firstName: firstNameValue,
-        lastName : lastNameValue,
-        phoneNumber : phoneNumberValue,
-        email : emailValue,
-        address : addressValue,
+        lastName: lastNameValue,
+        phoneNumber: phoneNumberValue,
+        email: emailValue,
+        address: addressValue,
     };
 
     state.contacts.push(contact);
@@ -109,17 +119,17 @@ function saveState() {
 // Rendering a map from Google Maps API
 
 // Converts address to Lat and Long values and renders Map
-function addressToMap(address){
+function addressToMap(address) {
     // API call to geocode the address. Added my API in the function instead of global variable to reduce merge conflicts
     fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + '&key=' + "AIzaSyDSVjMQM3Hgp3upVIWiHSW1CTTP-VFT85A")
-      .then(response => response.json())
-      .then(data => {
-        // Gets the address longitude and latitude coordinates
-        var lat = data.results[0].geometry.location.lat;
-        var lng = data.results[0].geometry.location.lng;
-        // Calls the render map function based on the coordinates of the given address
-        renderMap(lat,lng);
-      })
+        .then(response => response.json())
+        .then(data => {
+            // Gets the address longitude and latitude coordinates
+            var lat = data.results[0].geometry.location.lat;
+            var lng = data.results[0].geometry.location.lng;
+            // Calls the render map function based on the coordinates of the given address
+            renderMap(lat, lng);
+        })
 }
 
 // Initialize and add the map
@@ -135,8 +145,8 @@ function renderMap(lat, lng) {
     });
     // Creates a marker and puts it on the user contact's location on the map rendered above
     const marker = new google.maps.Marker({
-      position: contactLocation,
-      map: map,
+        position: contactLocation,
+        map: map,
     });
 }
 
